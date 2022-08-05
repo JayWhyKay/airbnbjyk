fa4876238aad354083f43146c38ae2abff9a7ceb
James K <jameskim369@gmail.com>
2022-08-05 11:53:15 -0700

    fixed spotbyid reviews

--- frontend/src/components/Reviews/index.js
+++ frontend/src/components/Reviews/index.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { loadSpotReviews } from "../../store/reviews";
+import { loadSpotReviews, removeReview } from "../../store/reviews";
+import ReviewModal from "../ReviewsModal";
 import "./Reviews.css";
 
 function Reviews({ spotId }) {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
   const reviews = useSelector((state) => Object.values(state.reviews));
+  const reviewUser = useSelector((state) => state.session.user);
   const spotReviews = reviews.filter(
     (review) => review.spotId === Number(spotId)
   );
@@ -28,8 +30,26 @@ function Reviews({ spotId }) {
           return (
             <div key={`review` + review.id} className="review_container">
               <div className="review_container__header">
+                <div>
                 <p>{review.User.firstName}</p>
                 <span>{dateToString(review.updatedAt)}</span>
+                </div>
+                { reviewUser && reviewUser.id === review.User.id && (
+                  <div className="spot_byid__review__button">
+                    <ReviewModal
+                      user={true}
+                      spotId={spotId}
+                      type="Edit"
+                      reviewId={review.id}
+                    />
+                    <button
+                      className="delete_review_button"
+                      onClick={() => dispatch(removeReview(review.id))}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                )}
               </div>
               <div>{review.review}</div>
             </div>
